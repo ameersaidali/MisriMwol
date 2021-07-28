3e238c72eadbe85b574bf59e61f3efc1f5688ae3
ash <87538672+ameersaidali@users.noreply.github.com>
2021-07-28 19:33:42 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -126,7 +126,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.blueBright.italic('🌈  Installing plugins...')
+            chalk.blueBright.italic('MisriMwol Installing plugins...')
         );
 
         fs.readdirSync('./plugins').forEach(plugin => {
@@ -136,7 +136,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.green.bold('Julie Mwol working! 😍')
+            chalk.green.bold('misri Mwol working! Happy alle😍')
         );
     });
     
@@ -240,8 +240,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~♥️🕊️______~ 𝕁𝕦𝕝𝕚𝕖𝕄𝕨𝕠𝕝 ~_____🕊️♥️~*' +
-                                    '\n\n*🧞‍♂️ ' + error + '*\n'
+                                await conn.sendMessage(conn.user.jid, '*~😍✨️______~ MisriMwol~_____✨️😍~*' +
+                                    '\n\n🧚‍♂️ ' + error + '*\n'
                                     , MessageType.text);
                             }
                         }
